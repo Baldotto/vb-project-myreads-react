ddc3d0f8416a717f3540daf382d6df74fdf1ff9f
baldotto <contato@viniciusbaldotto.com.br>
2017-12-16 10:57:14 -0200

    Refactoring
    Melhorando o código

--- src/Components/Books/Book.js
+++ src/Components/Books/Book.js
@@ -10,17 +10,17 @@ class Book extends Component{
   }
 
     render(){
-      //console.log('Props',this.props)
-      //const { books } = this.props
+
+      const { book } = this.props
 
         return (        
           <div className="book" >
              <div className="book-top">
-               <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url(' + this.props.book.imageLinks.smallThumbnail + ')' }}></div>
-               <BookShelfChanger  OnChangeBookShelf={this.props.OnChangeBookShelf} book={this.props.book} />
+               <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url(' + book.imageLinks.smallThumbnail + ')' }}></div>
+               <BookShelfChanger  OnChangeBookShelf={this.props.OnChangeBookShelf} book={book} />
              </div>
-             <div className="book-title">{this.props.book.title}</div>
-             <div className="book-authors">{this.props.book.authors}</div>
+             <div className="book-title">{book.title}</div>
+             <div className="book-authors">{book.authors}</div>
            </div>
             
         ) 
